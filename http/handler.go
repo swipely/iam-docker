794077e49b0fc7d37eaf77aaedc93e5333262f74
Tom Hulihan <hulihan.tom159@gmail.com>
2016-02-06 19:15:42 -0500

    Add logging to the http package

--- http/handler.go
+++ http/handler.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"encoding/json"
+	"github.com/Sirupsen/logrus"
 	"github.com/swipely/iam-docker/docker"
 	"github.com/swipely/iam-docker/iam"
 	"net/http"
@@ -19,36 +20,53 @@ const (
 
 var (
 	iamRegex = regexp.MustCompile("^/[^/]+/meta-data/iam/security-credentials/")
+	log      = logrus.WithFields(logrus.Fields{"package": "http"})
 )
 
 // NewIAMHandler creates a http.Handler which responds to metadata API requests.
 // When the request is for the IAM path, it looks up the IAM role in the
 // container store and fetches those credentials. Otherwise, it acts as a
 // reverse proxy for the real API.
-func NewIAMHandler(realIAMServer *url.URL, containerStore docker.ContainerStore, credentialStore iam.CredentialStore) http.Handler {
+func NewIAMHandler(upstream *url.URL, containerStore docker.ContainerStore, credentialStore iam.CredentialStore) http.Handler {
 	return &httpHandler{
 		containerStore:  containerStore,
 		credentialStore: credentialStore,
-		reverseProxy:    httputil.NewSingleHostReverseProxy(realIAMServer),
+		reverseProxy:    httputil.NewSingleHostReverseProxy(upstream),
 	}
 }
 
 func (handler *httpHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	logger := log.WithFields(logrus.Fields{
+		"path":   request.URL.Path,
+		"method": request.Method,
+	})
 	if (request.Method == iamMethod) && iamRegex.MatchString(request.URL.Path) {
-		handler.serveIAMRequest(writer, request)
+		logger.Info("Serving IAM credentials request")
+		handler.serveIAMRequest(writer, request, logger)
 	} else {
+		logger.Info("Serving reverse proxy request")
 		handler.reverseProxy.ServeHTTP(writer, request)
 	}
 }
 
-func (handler *httpHandler) serveIAMRequest(writer http.ResponseWriter, request *http.Request) {
+func (handler *httpHandler) serveIAMRequest(writer http.ResponseWriter, request *http.Request, logger *logrus.Entry) {
+	logger = logger.WithFields(logrus.Fields{"remoteAddr": request.RemoteAddr})
+	logger.Debug("Fetching IAM role")
 	role, err := handler.containerStore.IAMRoleForIP(request.RemoteAddr)
 	if err != nil {
+		logger.WithFields(logrus.Fields{
+			"error": err.Error(),
+		}).Warn("Unable to find IAM role")
 		writer.WriteHeader(http.StatusNotFound)
 		return
 	}
+	logger = logger.WithFields(logrus.Fields{"role": role})
+	logger.Debug("Fetching credentials")
 	creds, err := handler.credentialStore.CredentialsForRole(role)
 	if err != nil {
+		logger.WithFields(logrus.Fields{
+			"error": err.Error(),
+		}).Warn("Unable to find credentials")
 		writer.WriteHeader(http.StatusInternalServerError)
 		return
 	}
@@ -62,10 +80,20 @@ func (handler *httpHandler) serveIAMRequest(writer http.ResponseWriter, request 
 		Token:           *creds.SessionToken,
 	})
 	if err != nil {
+		logger.WithFields(logrus.Fields{
+			"error": err.Error(),
+		}).Warn("Unable to serialize JSON")
 		writer.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	_, _ = writer.Write(response)
+	_, err = writer.Write(response)
+	if err != nil {
+		logger.WithFields(logrus.Fields{
+			"error": err.Error(),
+		}).Warn("Unable to write response")
+		return
+	}
+	logger.Info("Successfully responded")
 }
 
 type credentialResponse struct {
