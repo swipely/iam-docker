1aa63ebb9e54e3ce2367456d9ea2dfb049b09042
Tom Hulihan <hulihan.tom159@gmail.com>
2016-05-01 16:56:37 -0400

    Print the log level

--- src/log/formatter.go
+++ src/log/formatter.go
@@ -19,20 +19,28 @@ func (f *Formatter) Format(entry *logrus.Entry) ([]byte, error) {
 	if !ok {
 		return []byte{}, errors.New("Invalid log prefix")
 	}
-	_, err := buffer.WriteString(fmt.Sprint(entry.Time.Format(time.RFC3339), " [", escapeIfNeeded(prefix), "] ", entry.Message))
+	msg := fmt.Sprint(
+		levelString(entry.Level),
+		" ",
+		entry.Time.Format(time.RFC3339),
+		" [", escapeIfNeeded(prefix), "] ",
+		entry.Message,
+	)
+	_, err := buffer.WriteString(msg)
 
 	if err != nil {
 		return []byte{}, err
 	}
 
 	for k, v := range entry.Data {
+		if k == "prefix" {
+			continue
+		}
 		key := escapeIfNeeded(k)
 		val := escapeIfNeeded(fmt.Sprint(v))
-		if key != "prefix" {
-			_, err = buffer.WriteString(fmt.Sprint(" ", key, "=", val))
-			if err != nil {
-				return []byte{}, err
-			}
+		_, err = buffer.WriteString(fmt.Sprint(" ", key, "=", val))
+		if err != nil {
+			return []byte{}, err
 		}
 	}
 
@@ -42,6 +50,25 @@ func (f *Formatter) Format(entry *logrus.Entry) ([]byte, error) {
 	}
 
 	return buffer.Bytes(), nil
+}
+
+func levelString(level logrus.Level) string {
+	switch level {
+	case logrus.DebugLevel:
+		return "D"
+	case logrus.InfoLevel:
+		return "I"
+	case logrus.WarnLevel:
+		return "W"
+	case logrus.ErrorLevel:
+		return "E"
+	case logrus.FatalLevel:
+		return "F"
+	case logrus.PanicLevel:
+		return "P"
+	}
+
+	return "U"
 }
 
 func escapeIfNeeded(str string) string {
